769b1f4819973b76de4a9eac9dc49f1af64f77d9
Hirusha Jayarathne <it22311290@my.sliit.lk>
2025-04-02 15:36:04 +0530

    Fix deployment errors

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   assetPrefix: isProd ? "/hirusha/" : "",
+  basePath: isProd ? "/hirusha" : "",
   images: {
     unoptimized: true,
   },
